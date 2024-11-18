428414020f30912c334d8f17b3a0efbd4cdefdc4
BaptLab <baptiste.labaune@gmail.com>
2024-11-18 12:18:37 +0100

    update

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/chess_puzzle_maker", // Update this to your subdirectory
+  assetPrefix: "/chess_puzzle_maker", // Ensures assets are correctly loaded
+  trailingSlash: true, // Ensures URLs end with a slash for static hosting
   /* other config options here */
 };
 
