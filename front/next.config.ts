31a7ca180c385d7989104fedabe2799fa4867094
BaptLab <baptiste.labaune@gmail.com>
2025-06-20 13:27:23 +0200

    fix back bug

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "", // The subdirectory for static assets
-  assetPrefix: "", // Correctly point to where static files are hosted
-  trailingSlash: true, // Ensures URLs end with a slash for static hosting
+	output: "export",
+	env: {
+		NEXT_PUBLIC_API_URL: "https://www.chesspuzzlemaker.com/back_php/controllers/",
+	},
+	basePath: "", // The subdirectory for static assets
+	assetPrefix: "", // Correctly point to where static files are hosted
+	trailingSlash: true, // Ensures URLs end with a slash for static hosting
 };
 
 export default nextConfig;
