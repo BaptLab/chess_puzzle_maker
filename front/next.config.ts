7517819262e927c1600016a3521846d189cd340e
BaptLab <baptiste.labaune@gmail.com>
2025-06-20 13:48:02 +0200

    bug correction

--- front/next.config.ts
+++ front/next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
 	basePath: "", // The subdirectory for static assets
 	assetPrefix: "", // Correctly point to where static files are hosted
 	trailingSlash: true, // Ensures URLs end with a slash for static hosting
+	env: {
+		NEXT_PUBLIC_API_URL: "https://www.chesspuzzlemaker.com/back_php/controllers/",
+	},
 };
 
 export default nextConfig;
