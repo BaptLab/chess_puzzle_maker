6aac49d32828054d5ee5fa0c5ae1c054456cce55
BaptLab <baptiste.labaune@gmail.com>
2025-06-20 13:34:19 +0200

    push

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	env: {
-		NEXT_PUBLIC_API_URL: "https://www.chesspuzzlemaker.com/back_php/controllers/",
-	},
 	basePath: "", // The subdirectory for static assets
 	assetPrefix: "", // Correctly point to where static files are hosted
 	trailingSlash: true, // Ensures URLs end with a slash for static hosting
